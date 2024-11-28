88c2882e6912990ff4bff7c59cd541665323ec20
NGUYEN THI HONG NHUNG <143203553+NhungNguyen04@users.noreply.github.com>
2024-11-28 10:48:22 +0700

    director/classes & students

---
+++ Frontend/app/(app)/(director)/directorprofile/page.tsx
@@ -0,0 +1,10 @@
+'use client'
+import { DirectorProfileCard } from "@/components/director/director-profile-card"
+
+export default function DirectorProfile() {
+  return (
+    <div className="flex justify-center w-[78vw] h-[100vw]">
+        <DirectorProfileCard/>
+    </div>
+  )
+}
---
+++ Frontend/app/(app)/(student)/studentprofile/page.tsx
@@ -0,0 +1,9 @@
+'use client'
+import { StudentProfileCard } from "@/components/student/student-profile-card"
+export default function StudentProfile() {
+  return (
+    <div className="flex justify-center w-[78vw] h-[100vw]">
+        <StudentProfileCard/>
+    </div>
+  )
+}
